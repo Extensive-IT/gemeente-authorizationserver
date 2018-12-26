4b8fea1fcbfdddc00691b8753ff219f800f26f25
Jelle Aret <jelle.aret@extensive-it.com>
2018-12-26 22:19:18 +0100

    Travis Integration with new Dockerfile

--- docker_push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t extensiveit/gemeente-authorizationserver:latest .
-docker push extensiveit/gemeente-authorizationserver:latest
